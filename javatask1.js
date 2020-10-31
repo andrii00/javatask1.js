7a940a6ab7ad1a70bd567969fd3bcbbd9166de31
andrii00 <kachmarik00@gmail.com>
2020-11-01 00:01:02 +0200

    1 lesson

---
+++ javatask1.js
@@ -0,0 +1,140 @@
+// 1. Завдання
+//let str = "Привет";
+//let num = 123;
+//let flag = true;
+//let txt = "true";
+
+// 2. Завдання
+//let a1 = 5+3;
+//let a2 = 5-3;
+//let a3 = 5*3;
+//let a4 = 5/3;
+//let a5 = 5%3;
+//
+//console.log(a1);
+//console.log(a2);
+//console.log(a3);
+//console.log(a4);
+//console.log(a5);
+//
+
+// 3.Завдання
+//let a6 = 5%3;
+//let a7 = 3%5;
+//let a8 = 5+'3';
+//let a9 = '5'-3;
+//let a10 = 75+'кг';
+//
+//console.log(a6);
+//console.log(a7);
+//console.log(a8);
+//console.log(a9);
+//console.log(a10);
+
+// 4. Завдання
+//let height = 23;
+//let width = 10;
+//let s = height * width;
+//
+//console.log(s);
+
+// 5. Завдання
+//let heightC = 10;
+//let dC = 4;
+//let V = Math.PI*dC*dC*heightC;
+//console.log(V);
+
+// 6.Task
+//let m= Math.pow(4,2);
+//const n = Math.pow(3,2);
+//console.log(m + n);
+
+// 7.Task
+//let str="Hello World";
+//document.write(str);
+//alert(str);
+//console.log(str);
+
+// 8.Task
+//alert("Ваше ФИО\nВозраст\nХобби");
+
+// 9.Task
+//let str1=" Кто";
+//let str2=" ти";
+//let str3=" такой?";
+//let concatenation= str1.concat(str2).concat( str3);
+//document.write(concatenation);
+
+// 10.Task
+//let str = "20";
+//let a = 5;
+//document.write(str + a + "<br/>");
+//document.write(str - a + "<br/>");
+//document.write(str * "2" + "<br/>");
+//document.write(str / 2 + "<br/>");
+
+// 11. Task
+//console.log(parseInt("3.14")); // 3
+//console.log(parseInt("-7.875")); // -7
+//console.log(parseInt("435")); // 345
+//console.log(parseInt("ВАСЯ")); // NaN
+
+// 12. Task
+//let str = prompt("Enter something", "ho-ho")
+//console.log(str);
+
+// 13. Task
+//let window1 = +prompt("Число 1");
+//let window2 = +prompt("Число 2");
+//alert(window1+window2);
+
+// 14. Task
+//let name = prompt("Укажіть Ваші ПІБ");
+//let age = prompt("Скільки Вам років?");
+//alert(`Доброго дня, ${name}`);
+//alert(`Мої вітання, що Вам ${age}`);
+
+// ДОПОЛНИТЕЛЬНО
+
+// 1. Task
+//let a = prompt("1");
+//let b = prompt("2");
+//let c = prompt("3");
+//
+//if (a>b>c) {
+//console.log(c,b,a);
+//}
+
+// 2. Task
+//let color= prompt('Який колір?');
+//
+//if (color === 'green'){
+//    document.write('go')
+//} else if ( color === 'yellow'){
+//    document.write('stop')
+//}else if( color==='red'){
+//    document.write('no stop')
+//} else{
+//    document.write('i dont now')
+//}
+
+// 3. Task
+//let color = prompt("Color?");
+//let isRoadClear = confirm("Car?");
+//if( color === "green" && isRoadClear) {
+//    document.write("go")
+//} else if( color==="green"){
+//    document.write("stop")
+//} else if (color==="yellow" && isRoadClear){
+//    document.write("wait")
+//} else if( color==="yellow"){
+//    document.write("все рано жди")
+//} else if (color==="red" && isRoadClear){
+//    document.write("wait")
+//} else if( color === "red"){
+//    document.write("waiting")
+//} else if (color==="avariya"){
+//    document.write("do anything")
+//}
+
+
